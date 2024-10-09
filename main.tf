49d956108484aba78bd376a11819a00aea7f13b6
Yogesh.KR <yogeshkr167@gmail.com>
2024-10-09 14:37:03 +0530

    adding main.tf content

---
+++ main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+    region = 'us-east-1'
+}
+resource "aws_instance" "Test" {
+    ami = "ami-0866a3c8686eaeeba"
+    instance_type = "t2.micro"
+}
+output "instance_ip" {
+    value = aws_instance.Test.public_ip
+}
